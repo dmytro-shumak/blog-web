4a47ed2bab49ef3f6d49bb7768947d7353d4d422
Dmytro <dima06444@gmail.com>
2025-02-28 15:27:57 +0200

    refactor: clean up code formatting and remove unused styles in Editor and PostCard components

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-// import JoditEditor from "jodit-react";
-const JoditEditor = dynamic(() => import('jodit-react'), {ssr: false})
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
 import dynamic from "next/dynamic";
 import { useMemo, useRef, useState } from "react";
@@ -11,25 +10,26 @@ interface Props {
   defaultContent?: string;
 }
 
-export default function Editor({ onChange, defaultContent = '' } :Props) {
-	const editor = useRef(null);
-	const [content, setContent] = useState(defaultContent);
+export default function Editor({ onChange, defaultContent = "" }: Props) {
+  const editor = useRef(null);
+  const [content, setContent] = useState(defaultContent);
 
-	const config = useMemo(() => ({
-			readonly: false, // all options from https://xdsoft.net/jodit/docs/,
-			placeholder: 'Start typings...'
-		}),
-		[]
-	);
+  const config = useMemo(
+    () => ({
+      readonly: false, // all options from https://xdsoft.net/jodit/docs/,
+      placeholder: "Start typings...",
+    }),
+    []
+  );
 
-	return (
-		<JoditEditor
-			ref={editor}
-			value={content}
-			config={config}
-			tabIndex={1} // tabIndex of textarea
-			onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-			onChange={onChange}
-		/>
-	);
+  return (
+    <JoditEditor
+      ref={editor}
+      value={content}
+      config={config}
+      tabIndex={1} // tabIndex of textarea
+      onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+      onChange={onChange}
+    />
+  );
 }
